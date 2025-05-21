b99535df738a994307fd30981f608750042ddfed
Sopulu920 <michaelmorah91@gmail.com>
2025-05-21 12:06:23 +0100

    products

--- amazon-site/src/app/page.tsx
+++ amazon-site/src/app/page.tsx
@@ -1,9 +1,20 @@
 import Banner from "@/component/homeComponent/banner";
+import Product from "@/component/homeComponent/product";
 
 export default function Home() {
   return (
     <>
-      <Banner />
+      <div className="page">
+
+        <div className="page1">
+          <Banner />
+        </div>
+
+        <div className="page2">
+          <Product />
+        </div>
+
+      </div>
     </>
   );
 }
---
+++ amazon-site/src/component/homeComponent/product.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game2.jpg"
+import game1 from "../../asset/image/game/game3.jpg"
+import game1 from "../../asset/image/game/game4.jpg"
+import game1 from "../../asset/image/game/game5.jpg"
+import game1 from "../../asset/image/game/game6.jpg"
+import game1 from "../../asset/image/game/game7.jpg"
+import game1 from "../../asset/image/game/game.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+import game1 from "../../asset/image/game/game1.jpg"
+
+
+export default function Product() {
+    return (
+        <>
+            <div className="product">
+
+                <div className="product-card">
+
+                    <div>
+                        Gaming accessories
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div className="product-card">
+
+                </div>
+                <div className="product-card">
+
+                </div>
+                <div className="product-card">
+
+                </div>
+
+                <div className="product-card">
+
+                </div>
+                <div className="product-card">
+
+                </div>
+                <div className="product-card">
+
+                </div>
+                <div className="product-card">
+
+                </div>
+
+            </div>
+        </>
+    )
+}
